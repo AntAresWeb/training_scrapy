a227692c8e8575a75705446074b40c710967bc5b
AntAresWeb <davy.anre@yandex.ru>
2023-11-16 19:50:14 +0300

    commit 2023-11-16.03

--- training_scrapy/spiders/example.py
+++ training_scrapy/spiders/example.py
@@ -1,10 +1,20 @@
 import scrapy
 
 
-class ExampleSpider(scrapy.Spider):
-    name = "example"
-    allowed_domains = ["example.com"]
-    start_urls = ["https://example.com"]
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('a.tag::text').getall(),
+            }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            # Если ссылка нашлась, загружаем страницу по ссылке
+            # и вызываем метод parse() ещё раз.
+            yield response.follow(next_page, callback=self.parse)
